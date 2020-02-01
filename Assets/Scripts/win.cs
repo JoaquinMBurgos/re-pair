4e8719b8e8404c084f14b9a89ff068fe6f094d4d
TomasGR <tomasgimenezr@gmail.com>
2020-02-01 11:07:27 -0300

    Added win condition

---
+++ Assets/Scripts/win.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class win : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("You win");
+        SceneManager.LoadScene("Level01");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
